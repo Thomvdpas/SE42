d26778635be4c4c1387e86a74a393f064d1fa2b0
Martijn van der Pol <martijn.vanderpol@student.fontys.nl>
2017-05-16 20:14:55 +0200

    Added structure to the repo! Also assignment 2 is done and pushed. Setup for assignment 3 is added.

---
+++ Week 2 (Auction)/auction/src/main/java/auction/dao/UserDAOJPAImpl.java
@@ -0,0 +1,77 @@
+package auction.dao;
+
+import auction.domain.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class UserDAOJPAImpl implements UserDAO {
+
+    // EntityManager
+    EntityManager entityManager;
+
+    public UserDAOJPAImpl(EntityManager em) {
+        entityManager = em;
+    }
+
+    @Override
+    public int count() {
+        Query q = entityManager.createNamedQuery("User.count");
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+    @Override
+    public void create(User user) {
+        entityManager.getTransaction().begin();
+
+         if (findByEmail(user.getEmail()) != null) {
+            throw new EntityExistsException();
+        }
+
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void edit(User user) {
+        entityManager.getTransaction().begin();
+
+        if (findByEmail(user.getEmail()) == null) {
+            throw new IllegalArgumentException();
+        }
+
+        entityManager.merge(user);
+        entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public List<User> findAll() {
+        Query q = entityManager.createNamedQuery("User.getAll");
+        return q.getResultList();
+    }
+
+    @Override
+    public User findByEmail(String email) {
+
+        User foundUser = null;
+        Query q = entityManager.createNamedQuery("User.findByEmail", User.class);
+        q.setParameter("email", email);
+
+        try {
+            foundUser = (User) q.getSingleResult();
+        }
+
+        catch (NoResultException nre) {
+            // Ignore this because as per your logic this is ok!
+        }
+
+        return foundUser;
+    }
+
+    @Override
+    public void remove(User user) {
+        entityManager.getTransaction().begin();
+        entityManager.remove((entityManager.merge(user)));
+        entityManager.getTransaction().commit();
+    }
+}
